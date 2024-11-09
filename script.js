ecbc30fd722590fc828ec2c62262595dd23f0e53
Sreeremya S <97663856+sreeremyas@users.noreply.github.com>
2024-11-09 14:20:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Initialize ScrollReveal and set default options
-ScrollReveal().reveal('hero,.contact,.experience, .projects, .about,.skills', {
-    duration: 1200,         // animation duration in milliseconds
-    origin: 'bottom',       // start the animation from the bottom
-    distance: '110px',       // how far the element moves during animation
-    reset: true             // animation repeats on each scroll
-});
